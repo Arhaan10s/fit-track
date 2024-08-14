ea304bf45242331622f27b69e9abb1522bea653d
Arhaan10s <69954942+Arhaan10s@users.noreply.github.com>
2024-08-14 11:24:00 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>   // using BrowserRouter for routing pages 
+    <BrowserRouter>    
         <App />
     </BrowserRouter>
   </StrictMode>
